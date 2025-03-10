f66ab7c8e545afa1ac8c76096ff70b1388c333e8
unknown <ali-sougui.ousmane@etu.univ-tlse2.fr>
2025-03-10 01:16:30 +0100

    mise a jour

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: './', // Ajoute ça si tu as des erreurs de chargement de fichiers
 });
