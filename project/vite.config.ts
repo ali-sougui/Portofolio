6437353f9a52f097fac673a0977ac8521f09ec38
unknown <ali-sougui.ousmane@etu.univ-tlse2.fr>
2025-03-10 01:20:02 +0100

    mise a jour

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ajoute ça si tu as des erreurs de chargement de fichiers
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+  }
 });
